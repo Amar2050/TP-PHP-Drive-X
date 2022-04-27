500833aa827985ce7c36031fc7eec1a839e92485
Amar2050 <amarboutayeb@gmail.com>
2022-04-27 13:04:08 +0200

    Mise en place d'une structure de base
    Dossiers:
    drive-X-PHP
    - assets> img| style.css
    - partials> header.php footer.php
    cars.php
    data.repo.php
    drivers.php
    index.php

---
+++ Drive-X-PHP/cars.php
@@ -0,0 +1,19 @@
+<?php require_once "data.repo.php";
+// var_dump($cars); TEST
+?> 
+<?php require_once "partials/header.php" ?>
+
+<main class="container">
+
+    <h1 class="my-5">
+        <?= $carTitle ?>
+    </h1>
+
+
+</main>
+
+<?php require "partials/footer.php" ?>
+
+</body>
+
+</html>
---
+++ Drive-X-PHP/drivers.php
@@ -0,0 +1,18 @@
+<?php require_once "data.repo.php"; // var_dump($drivers); TEST ?>
+
+<?php require_once "partials/header.php" ?>
+
+<main class="container">
+
+    <h1 class="my-5">
+        <?= $driverTitle ?>
+    </h1>
+
+   
+</main>
+
+<?php require "partials/footer.php" ?>
+
+</body>
+
+</html>
